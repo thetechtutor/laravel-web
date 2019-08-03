1fc6c7850ce1ba45e1da8a5df41b6dddd8d19568
Gladys Obmerga <gladysobmerga19@gmail.com>
2019-08-03 12:54:51 +0800

    change text

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    The Tech Tutor
                 </div>
 
                 <div class="links">
